d6ee99c21fa93e09fdabe8d1e1d675b6999e6e71
AOKI Yuuto <aoki@u-ne.co>
2013-11-20 09:52:16 +0900

    Initial Commit

---
+++ RadialEntity/RadialEntity.h
@@ -0,0 +1,13 @@
+//
+//  RadialEntity.h
+//  RadialEntity
+//
+//  Created by irony on 2013/11/20.
+//  Copyright (c) 2013年 Limbate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RadialEntity : NSObject
+
+@end
---
+++ RadialEntity/RadialEntity.m
@@ -0,0 +1,13 @@
+//
+//  RadialEntity.m
+//  RadialEntity
+//
+//  Created by irony on 2013/11/20.
+//  Copyright (c) 2013年 Limbate Inc. All rights reserved.
+//
+
+#import "RadialEntity.h"
+
+@implementation RadialEntity
+
+@end
